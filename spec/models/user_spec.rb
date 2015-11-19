e863d66922b092d70c5f7ccefce6dd14e006536b
Elle Meredith <ellemeredith@gmail.com>
2015-11-19 17:40:46 -0500

    User#invite

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,17 @@
 require "rails_helper"
 
 describe User  do
+  it { is_expected.to validate_presence_of(:first_name) }
+  it { is_expected.to validate_presence_of(:email) }
+
+  describe "#invite" do
+    it "sends an email invitation" do
+      user = create(:user)
+      allow(UserInvitationJob).to receive(:perform_later)
+
+      user.invite
+
+      expect(UserInvitationJob).to have_received(:perform_later)
+    end
+  end
 end
